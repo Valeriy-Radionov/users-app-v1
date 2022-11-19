fd84130e4218b688ff876045e887586476d2d2b4
valera <traberker901@gmail.com>
2022-11-19 12:51:50 +0300

    features: add appHooks

---
+++ src/features/bll/reducers/appReducer.ts
@@ -0,0 +1,8 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+const slice = createSlice({
+  name: "app",
+  initialState: {},
+  reducers: {},
+})
+
+export const appReducer = slice.reducer
---
+++ src/features/bll/reducers/authReducer.ts
@@ -0,0 +1,8 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+const slice = createSlice({
+  name: "auth",
+  initialState: {},
+  reducers: {},
+})
+
+export const authReducer = slice.reducer
