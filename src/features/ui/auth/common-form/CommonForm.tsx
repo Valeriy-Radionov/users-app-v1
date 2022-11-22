c0fec516cf52fbd3761df475ec58420d84cef203
valera <traberker901@gmail.com>
2022-11-22 14:39:36 +0300

    features: SnackBar,  customized Login/Registration

--- src/features/ui/auth/common-form/CommonForm.tsx
+++ src/features/ui/auth/common-form/CommonForm.tsx
@@ -1,18 +1,26 @@
-import { Label, LoginRounded } from "@mui/icons-material"
-import { Grid, FormControl, FormLabel, FormGroup, TextField, FormControlLabel, Checkbox, Button, Paper } from "@mui/material"
+import { Button, FormControl, FormGroup, Grid, TextField } from "@mui/material"
 import { useFormik } from "formik"
-import React, { ReactNode } from "react"
+import React from "react"
+import { LoginDataType } from "../../../../api/authApi"
+import { NavigateButton } from "../../../../common/components/nav-button/NavigateButton"
+import { RouterPath } from "../../../../common/components/routes/Routs"
+import { useAppDispatch } from "../../../../common/hooks/storeHooks"
 import { validatorEmail, validatorRequiredValue } from "../../../../common/utils/validators/authValidators"
+import { loginTC } from "../../../bll/reducers/authReducer"
 export type FormikType = {
   email?: string
   password?: string
   name?: string
 }
 type CommonAuthFormType = {
+  rout: RouterPath.login | RouterPath.registr
+  navLinkName: string
   onValidatorUserName?: boolean
+  submitBtnname: string
 }
 
-export const CommonAuthForm: React.FC<CommonAuthFormType> = ({ onValidatorUserName }) => {
+export const CommonAuthForm: React.FC<CommonAuthFormType> = ({ onValidatorUserName, rout, navLinkName, submitBtnname }) => {
+  const dispatch = useAppDispatch()
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -21,32 +29,41 @@ export const CommonAuthForm: React.FC<CommonAuthFormType> = ({ onValidatorUserNa
     },
     validate: (values) => {
       const errors: FormikType = {}
-      errors.email = validatorEmail(values.email, errors).email
-      errors.password = validatorRequiredValue(values, errors).password
-      onValidatorUserName && (errors.name = validatorRequiredValue(values, errors).name)
+      validatorEmail(values.email, errors)
+      validatorRequiredValue(values, errors, onValidatorUserName)
+      console.log(errors)
       return errors
     },
     onSubmit: (values) => {
+      const value: LoginDataType = {
+        email: values.email,
+        password: values.password,
+      }
+      !onValidatorUserName && dispatch(loginTC(value))
+      console.log("On submit")
       formik.resetForm()
     },
   })
   return (
     <Grid container justifyContent={"center"} alignItems={"center"} sx={{ height: "40vh" }}>
       <Grid item justifyContent={"center"} alignItems={"center"}>
-        <form onSubmit={formik.handleSubmit}>
-          <FormControl sx={{ width: "300px" }}>
+        <form onSubmit={formik.handleSubmit} style={{ textAlign: "center" }}>
+          <FormControl sx={{ width: "300px", bgcolor: "#c5d0d1", marginTop: "50px", borderRadius: "5px" }}>
             <FormGroup sx={{ margin: "20px" }}>
               {onValidatorUserName ? <h2 style={{ textAlign: "center" }}>Enter your registration data</h2> : <h2>Enter your email address and password</h2>}
               {onValidatorUserName && <TextField label="User name" margin="normal" {...formik.getFieldProps("name")} />}
-              {formik.touched.name && formik.errors.name && <div style={{ color: "red", padding: "0px 0px 10px 5px" }}>{formik.errors.name}</div>}
+              {formik.touched.name && formik.errors.name && onValidatorUserName && <div style={{ color: "red", padding: "0px 0px 10px 5px" }}>{formik.errors.name}</div>}
+
               <TextField label="Email" margin="normal" {...formik.getFieldProps("email")} />
               {formik.touched.email && formik.errors.email && <div style={{ color: "red", padding: "0px 0px 10px 5px" }}>{formik.errors.email}</div>}
+
               <TextField type="password" label="Password" margin="normal" {...formik.getFieldProps("password")} />
               {formik.touched.password && formik.errors.password && <div style={{ color: "red", padding: "0px 0px 10px 5px" }}>{formik.errors.password}</div>}
               <Button type={"submit"} variant={"contained"} sx={{ bgcolor: "#6a77d9" }}>
-                Login
+                {submitBtnname}
               </Button>
             </FormGroup>
+            <NavigateButton name={navLinkName} rout={rout} />
           </FormControl>
         </form>
       </Grid>
