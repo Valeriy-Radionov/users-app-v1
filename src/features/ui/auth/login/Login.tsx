42c5b5052414c36cc65796d481267e6b54814b86
valera <traberker901@gmail.com>
2022-11-25 16:34:53 +0300

    bugFix: blockApi

--- src/features/ui/auth/login/Login.tsx
+++ src/features/ui/auth/login/Login.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react"
 import { RouterPath } from "../../../../common/components/routes/Routs"
+import { useAppDispatch } from "../../../../common/hooks/storeHooks"
+import { deleteRegData } from "../../../bll/reducers/authReducer"
 import { CommonAuthForm } from "../common-form/CommonForm"
 
 export const Login = () => {
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(deleteRegData({ value: false }))
+  }, [])
+
   return <CommonAuthForm navLinkName="Registration" rout={RouterPath.registr} submitBtnname="LOGIN" onValidatorUserName={false} />
 }
